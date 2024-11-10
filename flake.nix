aa9fc46166b5c9abd5d1497a096c34176743a565
DormBrand <dorm.brand@gmail.com>
2024-11-11 00:51:43 +0100

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "My nixos dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ...}@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in {
+      nixosConfigurations.amber = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./hosts/amber/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.julius = import ./home-manager/home.nix;
+          }
+        ];
+      };
+    };
+}
---
+++ hosts/amber/configuration.nix
@@ -0,0 +1,77 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.graceful = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd = {
+    network = {
+      enable = true;
+      udhcpc.enable = true;
+      flushBeforeStage2 = true;
+      ssh = {
+        enable = true;
+        port = 2222;  # Use a separate port to prevent conflict in your known
+                      # hosts
+        shell = "/bin/cryptsetup-askpass";
+        hostKeys = [ /etc/secrets/initrd/ssh_host_ed25519_key ];
+        authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9QDOf9w9OojC2ZGYsH4r1UOacEKyOZUeMVwuGlMsGY julius@DESKTOP-5A890" ];
+      };
+    };
+    availableKernelModules = [ "e1000e" ];
+  };
+  
+  networking = {
+    hostName = "amber";
+    networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "de";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.julius = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9QDOf9w9OojC2ZGYsH4r1UOacEKyOZUeMVwuGlMsGY julius@DESKTOP-5A890JT"
+    ];
+    initialPassword = "justthebeginningofeverything";
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  system.stateVersion = "24.11";
+
+}
